3528cb583e15b205bcb6577ee113c5c5914079e0
Daniel W <drwells0109@gmail.com>
2025-02-06 16:22:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+  for(i = 0; i <2; i++)
+    {
+      //do stuff
+      console.log(i); 
+      $("#bedOne").append('<div class="pillow">pillow</div>');
+      $("#bedTwo").append('<div class="pillow">pillow</div>');
+    
+    }
+
+    $("#Funny-seal").draggable(); 
+
+// Help from JqueryUI.com and w3Schools
+$(document).on("mouseup", (event) => {
+  if (event.target.id === "Funny-seal") {
+    const position = $("#Funny-seal").position();
+    console.log("Your position is:", position.left, "and", position.top);
+  }
+});
